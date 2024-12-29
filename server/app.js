3495350094f248aac358f4d1d79999b2c7d90c48
hcmlweb <hcml.web@gmail.com>
2024-12-29 19:18:28 +0600

    server deleted vercel add

--- server/app.js
+++ server/app.js
@@ -1,8 +1,16 @@
 
+
+
+require('dotenv').config(); 
 const express = require('express')
 const cors=require('cors')
 const app=express()
-const expanceRouter = require('./router/expanceRouter')
+
+const PORT = process.env.PORT || 4001;
+const MONGO_URI = process.env.MONGO_URI;
+
+
+
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
@@ -10,6 +18,25 @@ app.use(cors({
   origin:'http://localhost:5173',
   methods:['GET','POST']
 }));
+
+
+
+
+const expanceRouter = require('./router/expanceRouter')
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((error) => console.error('Error connecting to MongoDB:', error));
+
+
+
+  app.listen(PORT,()=>{
+      console.log(`Server is running at ${PORT}`)
+  })
+
+
 
 app.use('/api/expance', expanceRouter)
 
