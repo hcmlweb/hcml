3d35b7f3ab7698eb090a44165e1551bc39f3ae8c
hcmlweb <hcml.web@gmail.com>
2024-12-30 13:34:23 +0600

    griege router

--- server/schema/griege.schema.js
+++ server/schema/griege.schema.js
@@ -2,9 +2,12 @@ const mongoose=require('mongoose')
 const griegeSchema=mongoose.Schema({
 lotNumber:{
     type:Number,
-    require:true,
-    date:{
-        type:date,
-    }
+    require:true
+},
+date:{
+    type:date,
 }
-})
+}
+)
+
+module.exports=griegeSchema;
