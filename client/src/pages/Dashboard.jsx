07ccf2229b4c9b9d5547ded7b5a4011f6718483a
hcmlweb <hcml.web@gmail.com>
2025-01-09 18:57:12 +0600

    Dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Logo from '../assets/logo.png'
 
 function Dashboard() {
   return (
     <>
         <div className='w-full h-screen flex flex-col items-center justify-center space-y-4 '>
+            <img src={Logo} alt="HCML" className='w-[150px]' />
             <div>Dashboard</div>
             <Link to="/lot" className='py-2 px-8 bg-green-700 text-white'>Enter</Link>
         </div>
