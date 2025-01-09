0269521c52eab27a82ed2e6a0314b8e80e2551b6
hcmlweb <hcml.web@gmail.com>
2025-01-09 17:13:53 +0600

    Dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,14 @@ import Lot from "./pages/Lot"
 import LotForm from "./components/LotForm"
 import PartyForm from "./components/PartyForm"
 import LotManage from "./components/LotManage"
+import Dashboard from "./pages/Dashboard"
 function App() {
 
 
   return (
     <>
       <Routes>
+        <Route path="/" element={<Dashboard />} />
         <Route path="/lot" element={<Lot />} />
         <Route path="/newlot" element={<LotForm />} />
         <Route path="/party" element={<PartyForm />} />
