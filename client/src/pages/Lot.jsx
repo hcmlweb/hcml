f1cb23e0e2e4b5bbd3bf3054a3e4abf3a8ae480a
hcmlweb <hcml.web@gmail.com>
2025-01-09 18:47:05 +0600

    Dashboard

--- client/src/pages/Lot.jsx
+++ client/src/pages/Lot.jsx
@@ -28,7 +28,7 @@ const Lot = () => {
 
     return (
         <>
-            <div className="w-full flex flex-col items-center justify-between md:p-4">
+            <div className="w-full flex flex-col items-center justify-between py-4 md:p-4">
                 <div className="uppercase">
                     <h2 className="border-[1px] border-blue-500 py-2 px-4 rounded-md text-sm font-semibold shadow-md">Lot Management</h2>
                 </div>
