2901cc0c61c043baaa9a14e0eafe0ff942b33e96
hcmlweb <hcml.web@gmail.com>
2025-01-09 17:18:09 +0600

    Dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Dashboard() {
   return (
-    <div>Dashboard</div>
+    <>
+        <div className='w-full h-screen flex flex-col items-center justify-center space-y-4 '>
+            <div>Dashboard</div>
+            <Link to="/lot" className='py-2 px-8 bg-green-700 text-white'>Enter</Link>
+        </div>
+    </>
+    
   )
 }
 
