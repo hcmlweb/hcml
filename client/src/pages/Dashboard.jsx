a26ed9eda8209a02769d2fe5a03d3b9457c0dc70
hcmlweb <hcml.web@gmail.com>
2025-01-09 19:15:04 +0600

    Dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -6,7 +6,7 @@ function Dashboard() {
   return (
     <>
         <div className='w-full h-screen flex flex-col items-center justify-center space-y-4 '>
-            <img src={Logo} alt="HCML" className='w-[150px]' />
+            <img src={Logo} alt="HCML" className='w-[80px]' />
             <div>Dashboard</div>
             <Link to="/lot" className='py-2 px-8 bg-green-700 text-white'>Enter</Link>
         </div>
