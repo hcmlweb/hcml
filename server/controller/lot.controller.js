91007d0b9c6fac0438f604b078f621a18d8047df
hcmlweb <hcml.web@gmail.com>
2025-01-11 11:34:36 +0600

    Lot Date Fix

--- server/controller/lot.controller.js
+++ server/controller/lot.controller.js
@@ -30,7 +30,7 @@ const createLot = async (req, res) => {
             deliverFabrics: 0,
             availableFabrics: 0,
             lotStatus: "Receive Griege",
-            date: addNewDate ? addNewDate : utcPlusSixHours
+            date: addNewDate
         })
         const saveLot = await newLot.save()
         res.status(201).json(saveLot)
