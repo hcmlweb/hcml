4a8a04410da888f1338d2783a358d3eb251053e5
hcmlweb <hcml.web@gmail.com>
2025-01-11 15:34:00 +0600

    Adjust Table

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -56,7 +56,7 @@ function Dashboard() {
             <div className="uppercase">
               <h2 className="border-[1px] border-blue-500 py-2 px-4 rounded-md text-sm font-semibold shadow-md text-center">Dashboard</h2>
             </div>
-            <div className='grid grid-cols-2 item-cemter justify-between mt-1 sm:mt-2'>
+            <div className='grid grid-cols-1 md:grid-cols-2 item-cemter justify-between mt-1 sm:mt-2'>
               <div className='col-span-1 flex flex-col items-center justify-center'>
                 <h2 className='w-full px-2 py-1 font-medium text-sm shadow-md  text-center uppercase border-[1px] border-blue-500'>Todays Griege In</h2>
                 <ul className='w-full grid grid-cols-4 items-center justify-center border-x-[1px] border-y-0 border-gray-800 bg-blue-600 text-white text-center text-sm'>
@@ -71,8 +71,7 @@ function Dashboard() {
                     return(
                       <ul className='w-full grid grid-cols-4 items-center justify-center border-x-[1px] border-y-0 border-blue-600 border-t-0 border-b-[1px]  text-center  text-sm'>
                         <li className=' pl-[1px] col-span-1'>{party.lotNumber}</li>
-                        <li className=' pl-[1px] col-span-1'>{party.partyName}</li>
-                        
+                        <li className=' pl-[1px] col-span-1'>{party.partyName}</li> 
                         <li className=' pl-[1px] col-span-1'>{party.fabrics.reduce((total, fabric) => total + fabric.fabricAmount, 0)}</li>
                         <li className=' pl-[1px] col-span-1'>{party.fabrics.reduce((total, than) => total + than.thanQty, 0)}</li>
                       </ul>
