fdce2971e139e15cfed4b26142c6c80d44b81c59
hcmlweb <hcml.web@gmail.com>
2025-01-13 08:47:57 +0600

    Update color.model.js

--- server/model/color.model.js
+++ server/model/color.model.js
@@ -1,4 +1,4 @@
 const mongoose=require('mongoose')
 const colorSchema = require('../schema/color.schema')
 const colorModel=mongoose.model('color',colorSchema)
-module.exports={colorModel}
+module.exports=colorModel;
