8bc8668d7b1caab9e4aca8de2ad2d9176b413bfa
hcmlweb <hcml.web@gmail.com>
2025-01-19 00:43:09 +0600

    Update Dashboard.jsx

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -109,7 +109,7 @@ function Dashboard() {
         <div className='w-full flex flex-col items-center justify-center mt-4'>
           <h2 className="border-[1px] border-blue-500 py-2 px-4 rounded-md text-sm font-semibold shadow-md text-center">Dyeing Today</h2>
           <div className='py-2 w-full'>
-            <ul className='w-full px-4 py-1 bg-green-500 text-white grid grid-cols-8 item-center justify-between uppercase text-xs md:text-sm font-semibold'>
+            <ul className='w-full px-4 py-1 bg-green-500 text-white grid grid-cols-8 item-center justify-between capitalized text-xs md:text-sm font-semibold'>
               <li className='col-span-1'>lot number</li>
               <li className='col-span-2'>party name</li>
               <li className='col-span-1'>memo</li>
@@ -120,7 +120,7 @@ function Dashboard() {
             </ul>
             {collectDemand.map((item, i) => {
               return (
-                <ul className={`w-full px-4 py-1 ${i % 2 ? "bg-white" : "bg-gray-300"}  grid grid-cols-8 item-center justify-between uppercase text-xs md:text-sm`}>
+                <ul className={`w-full px-4 py-1 ${i % 2 ? "bg-white" : "bg-gray-300"}  grid grid-cols-8 item-center justify-between capitalized text-xs md:text-sm`}>
                   <li className='col-span-1'>{item.lotNumber}</li>
                   <li className='col-span-2'>{item.partyName}</li>
                   <li className='col-span-1'>{item.memoNumber}</li>
