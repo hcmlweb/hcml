e35890a20f93313ad4279d56ce2a19d8b7a4e82c
hcmlweb <hcml.web@gmail.com>
2025-01-24 12:31:26 +0600

    Update AllLots.jsx

--- client/src/components/grieges/AllLots.jsx
+++ client/src/components/grieges/AllLots.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+
 
 function AllLots() {
+  const [lot, setLot]=useState([])
+
+useEffect(()>={
+  fetch('https://hcml-d4nk.vercel.app/api/lot')
+   .then(res=>res.json())
+   .then(data=>setLot(data))
+},[])
+
+
+  
   return (
-    <div>AllLots</div>
+    <div className="p-4">
+     <h2>All Lot's</h2>
+      {lot.map(item=>{
+      return(
+        <h3>{item.lotNumber}</h3>
+      )
+      })}
+    </div>
   )
 }
 
