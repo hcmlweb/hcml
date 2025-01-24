670e8207f74d168015405544d3ba5a15c76084c5
hcmlweb <hcml.web@gmail.com>
2025-01-24 12:16:59 +0600

    Update Lot.jsx

--- client/src/pages/Lot.jsx
+++ client/src/pages/Lot.jsx
@@ -35,11 +35,11 @@ const Lot = () => {
                     <h2 className="border-[1px] border-blue-500 py-2 px-4 rounded-md text-sm font-semibold shadow-md">Lot Management</h2>
                 </div>
                 <div className="w-full">
-                    <div className="w-full flex flex-row items-start justify-start divide-x-[1px]">
-                        <NavLink to="alllot" className={({isActive})=>isActive? "bg-gray-400 px-4 py-1":"bg-gray-300 px-4 py-1"}>All Lots</NavLink>
+                    <div className="w-full flex flex-row items-start justify-start divide-x-[1px] border-b-1 border-gray-800">
+                        <NavLink to="alllot" className={({isActive})=>isActive? "bg-gray-400 px-4 py-1 rounded-t-md":"bg-gray-300 px-4 py-1 rounded-t-md"}>All Lots</NavLink>
                         
-                        <NavLink to="deliverylots" className={({isActive})=>isActive? "bg-gray-400 px-4 py-1":"bg-gray-300 px-4 py-1"}>Delivery Lots</NavLink>
-                        <NavLink to="closelots" className={({isActive})=>isActive? "bg-gray-400 px-4 py-1":"bg-gray-300 px-4 py-1"}>Close Lots</NavLink>
+                        <NavLink to="deliverylots" className={({isActive})=>isActive? "bg-gray-400 px-4 py-1 rounded-t-md":"bg-gray-300 px-4 py-1 rounded-t-md"}>Delivery Lots</NavLink>
+                        <NavLink to="closelots" className={({isActive})=>isActive? "bg-gray-400 px-4 py-1 rounded-t-md":"bg-gray-300 px-4 py-1 rounded-t-md"}>Close Lots</NavLink>
                     </div>
                     <main>
                         <Outlet />
