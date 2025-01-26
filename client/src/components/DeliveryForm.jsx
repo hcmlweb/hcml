daa1b99144f0e4421c5fc2cc416470e0df544192
hcmlweb <hcml.web@gmail.com>
2025-01-26 23:29:56 +0600

    Update DeliveryForm.jsx

--- client/src/components/DeliveryForm.jsx
+++ client/src/components/DeliveryForm.jsx
@@ -9,14 +9,8 @@ function DeliveryForm({ id, setDeliveryModal }) {
     e.preventDefault();
     setIsLoading(true);
 
-    if (!fabricAmount || fabricAmount <= 0 || !thanQty || thanQty <= 0) {
-      alert("Please enter valid values.");
-      setIsLoading(false);
-      return;
-    }
-
     try {
-      const response = await fetch(`https://hcml-d4nk.vercel.app/api/deliver/${id}`, {
+      const response = await fetch(`https://hcml-d4nk.vercel.app/api/lot/deliver/${id}`, {
         method: "POST",
         body: JSON.stringify({ fabricAmount, thanQty }),
         headers: { "Content-Type": "application/json" },
